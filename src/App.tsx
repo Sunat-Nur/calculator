09d6db7950e9307543058edb267c5d4a1f264ae0
Sunat_nur <uzbekistan9699@gmail.com>
2024-04-02 14:51:45 +0900

    BRR: Initialize project using create react app

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import {Container, Stack} from "@mui/material";
+
+function App() {
+    return (
+        <div className="calculator_div">
+            <Container>
+                <Stack>
+
+                </Stack>
+            </Container>
+
+        </div>
+    );
+}
+
+export default App;
